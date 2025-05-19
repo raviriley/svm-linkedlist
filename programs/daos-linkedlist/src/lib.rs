66ef77032403cfec4d41e4862915a24fe1845d05
Ravi Riley <raviriley@gmail.com>
2025-05-19 00:05:59 -0700

    Initialize, insert, & delete

--- programs/daos-linkedlist/src/lib.rs
+++ programs/daos-linkedlist/src/lib.rs
@@ -7,7 +7,51 @@ pub mod daos_linkedlist {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let linked_list = &mut ctx.accounts.linked_list;
+        linked_list.head = None;
+        Ok(())
+    }
+
+    pub fn insert(ctx: Context<Insert>, data: u8) -> Result<()> {
+        let node = &mut ctx.accounts.node;
+        let linked_list = &mut ctx.accounts.linked_list;
+
+        node.data = data;
+
+        // if there exists an after_node, we are inserting after a specific node and need to link the new node to the next node
+        if let Some(after_node) = ctx.accounts.after_node.as_mut() {
+            node.next = after_node.next;
+            after_node.next = Some(node.key());
+        } else {
+            // Otherwise, we are inserting at the head
+            node.next = linked_list.head;
+            linked_list.head = Some(node.key());
+        }
+
+        Ok(())
+    }
+
+    pub fn delete(ctx: Context<Delete>) -> Result<()> {
+        let linked_list = &mut ctx.accounts.linked_list;
+        let node_to_delete = &ctx.accounts.node_to_delete;
+
+        // if removing the head, we need to update the head to the next node
+        if let Some(head) = linked_list.head {
+            if head == node_to_delete.key() {
+                linked_list.head = node_to_delete.next;
+                return Ok(());
+            }
+        }
+
+        // if removing a node in the middle/end, we need to connect the previous node to the node after the one being deleted
+        if let Some(prev_node) = ctx.accounts.prev_node.as_mut() {
+            if let Some(next_key) = prev_node.next {
+                if next_key == node_to_delete.key() {
+                    prev_node.next = node_to_delete.next;
+                }
+            }
+        }
+
         Ok(())
     }
 }
